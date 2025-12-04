5968391ecb57ea19dcbbf526e3cf3ac5a145ceed
SSpirate11 <sanjaisiddharth2002@gmail.com>
2025-12-05 03:16:01 +0530

    Fix: Apply automated fix to utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 function greet(name) {
-  return "Hello, " + name
+  return 'Hello, ' + name;
 }
 
 function farewell(name) {
-  return "Goodbye, " + name
+  return 'Goodbye, ' + name;
 }
 
-module.exports = { greet, farewell }
+module.exports = { greet, farewell };
